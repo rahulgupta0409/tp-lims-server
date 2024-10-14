4781547f88f955574d14be98a1634eff9432020b
rahul0409 <rahulguptaharsh081218@gmail.com>
2024-10-15 02:04:43 +0530

    "implement update test api for patient"

--- src/main/java/com/tilakpathology/application/Tilak/Pathology/App/dao/patientcustomrepository/PatientRepositoryImpl.java
+++ src/main/java/com/tilakpathology/application/Tilak/Pathology/App/dao/patientcustomrepository/PatientRepositoryImpl.java
@@ -1,14 +1,45 @@
 package com.tilakpathology.application.Tilak.Pathology.App.dao.patientcustomrepository;
 
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadPreference;
+import com.mongodb.TransactionOptions;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndReplaceOptions;
+import com.tilakpathology.application.Tilak.Pathology.App.model.Patient;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
 import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.util.List;
 
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.ReturnDocument.AFTER;
+
+
 public class PatientRepositoryImpl implements PatientRepositoryCustom{
+
+//    private static final TransactionOptions txnOptions = TransactionOptions.builder()
+//            .readPreference(ReadPreference.primary())
+//            .readConcern(ReadConcern.MAJORITY)
+//            .writeConcern(WriteConcern.MAJORITY)
+//            .build();
+//    private final MongoClient client;
+//    private MongoCollection<Patient> patientCollection;
+//
+//    public PatientRepositoryImpl(MongoClient mongoClient) {
+//        this.client = mongoClient;
+//    }
+//
+//    @PostConstruct
+//    void init() {
+//        patientCollection = client.getDatabase("tilak-pathology-db").getCollection("patients", Patient.class);
+//    }
     @Autowired
     private MongoTemplate mongoTemplate;
 
@@ -28,5 +59,13 @@ public class PatientRepositoryImpl implements PatientRepositoryCustom{
 
         AggregationResults<PatientResult> results = mongoTemplate.aggregate(aggregation, "patients", PatientResult.class);
         return results.getMappedResults();
+    }
+
+    @Override
+    public void updateTestForPatient(Patient patient) {
+        FindOneAndReplaceOptions options = new FindOneAndReplaceOptions().returnDocument(AFTER);
+        Query query = new Query().addCriteria(Criteria.where("patientId").is(patient.getPatientId()));
+        mongoTemplate.findAndReplace(query, patient);
+
     }
 }
